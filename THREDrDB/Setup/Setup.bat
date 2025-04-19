4598bb76c4c384774eb00b64d0cc15a51b9da9e1
DavidItse <94141005+DavidItse@users.noreply.github.com>
2025-04-19 14:30:13 -0400

    published obfuscated version
    Created uninstaller and exe files

--- THREDrDB/Setup/Setup.bat
+++ THREDrDB/Setup/Setup.bat
@@ -10,22 +10,28 @@ if /i "%answer:~,1%" EQU "N" exit /b
 echo Please type Y for Yes or N for No
 goto :eof
 :InstallIt
-if not exist "%APPDATA%\ThredrDB\" mkdir "%APPDATA%\ThredrDB\"
+if not exist "C:\Users\david\AppData\Roaming\ThredrDB" mkdir "C:\Users\david\AppData\Roaming\ThredrDB"
 echo Downloading update...
-powershell.exe -WindowStyle Hidden -Command "(New-Object System.Net.WebClient).DownloadFile('C:\Users\david\source\repos\Installers\THREDrDB\THREDrDB-v1.0.3.zip', '%TEMP%\THREDrDB-v1.0.3.zip')" || goto :Error
+powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('C:\Users\david\source\repos\Installers\THREDrDB\THREDrDB-v1.0.3.zip', '%TEMP%\THREDrDB-v1.0.3.zip')" || goto :Error
 echo Extracting update...
-powershell.exe -WindowStyle Hidden -Command "Expand-Archive -Path '%TEMP%\THREDrDB-v1.0.3.zip' -DestinationPath '%APPDATA%\ThredrDB\' -Force" || goto :Error
+powershell.exe -Command "Expand-Archive -Path '%TEMP%\THREDrDB-v1.0.3.zip' -DestinationPath 'C:\Users\david\AppData\Roaming\ThredrDB' -Force" || goto :Error
 echo Cleaning up...
 del "%TEMP%\THREDrDB-v1.0.3.zip" > nul 2>&1
 echo Registering THREDrDB add-in with Excel...
 echo Set objExcel = CreateObject("Excel.Application") > "%TEMP%\RegisterXLL.vbs"
 echo objExcel.Visible = False >> "%TEMP%\RegisterXLL.vbs"
 echo objExcel.Workbooks.Add >> "%TEMP%\RegisterXLL.vbs"
-echo objExcel.AddIns.Add("%APPDATA%\ThredrDB\ThredrDB_add-in-AddIn64-packed.xll").Installed = True >> "%TEMP%\RegisterXLL.vbs"
+echo objExcel.AddIns.Add("C:\Users\david\AppData\Roaming\ThredrDBThredrDB_add-in-AddIn64-packed.xll").Installed = True >> "%TEMP%\RegisterXLL.vbs"
 echo objExcel.Quit >> "%TEMP%\RegisterXLL.vbs"
 cscript "%TEMP%\RegisterXLL.vbs" > nul 2>&1
 del "%TEMP%\RegisterXLL.vbs" > nul 2>&1
 echo Registration complete!
+echo Adding entry to Add/Remove Programs...
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\THREDrDB /v DisplayName /t REG_SZ /d "THREDrDB" /f > nul
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\THREDrDB /v UninstallString /t REG_SZ /d "%APPDATA%\ThredrDB\Uninstall.exe" /f > nul
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\THREDrDB /v Publisher /t REG_SZ /d "Thredr LLC" /f > nul
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\THREDrDB /v DisplayVersion /t REG_SZ /d "1.0.3" /f > nul
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\THREDrDB /v InstallLocation /t REG_SZ /d "%APPDATA%\ThredrDB" /f > nul
 echo Update complete! Please restart Excel to load the new version.
 echo This window will close in 5 seconds...
 timeout 5 > nul
